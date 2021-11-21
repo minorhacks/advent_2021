ddd5b76d6c5c0185d2d58988a05bb83fcd093c0e
Scott Minor <minor@minorhacks.com>
2021-11-20 21:09:56 -0800

    Can run day_01 via `bazel run`

--- WORKSPACE
+++ WORKSPACE
@@ -1,3 +1,5 @@
+workspace(name = "advent_2018")
+
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 ################################################################################
--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,22 @@
+use std::env;
 use std::fs;
 use std::io;
 use std::path;
 
-pub fn open<T: AsRef<path::Path>>(filename: T) -> std::io::Result<io::BufReader<fs::File>> {
-    Ok(io::BufReader::new(fs::File::open(filename)?))
+pub fn open(filename: &str) -> std::io::Result<io::BufReader<fs::File>> {
+    Ok(io::BufReader::new(fs::File::open(runfile(filename))?))
 }
 
 pub fn remove_empty_lines(r: &Result<String, io::Error>) -> bool {
     match r {
         Err(_) => true,
         Ok(l) => !l.is_empty(),
+    }
+}
+
+pub fn runfile(p: &str) -> path::PathBuf {
+    match env::var("RUNFILES_DIR") {
+        Ok(runfiles_path) => path::PathBuf::from(runfiles_path).join(p),
+        Err(_) => path::PathBuf::from(p),
     }
 }
