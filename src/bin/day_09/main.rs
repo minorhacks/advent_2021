af49fa691fbb71dd922974809506fb1a218fad30
Scott Minor <minor@minorhacks.com>
2021-12-10 16:56:10 -0800

    Finished day 09

--- src/bin/day_09/main.rs
+++ src/bin/day_09/main.rs
@@ -5,5 +5,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let height_map = input.parse::<advent_2021::lava_tube::HeightMap>()?;
     println!("Part 1: {}", height_map.risk_level_sum());
 
+    println!("Part 1: {}", height_map.largest_basins_product(3));
     Ok(())
 }
--- src/lava_tube.rs
+++ src/lava_tube.rs
@@ -1,6 +1,7 @@
 use anyhow::anyhow;
 use anyhow::Result;
 use itertools::Itertools;
+use std::collections::{HashSet, VecDeque};
 
 type Height = u8;
 pub struct HeightMap(array2d::Array2D<Height>);
@@ -40,7 +41,7 @@ impl std::fmt::Display for HeightMap {
 impl HeightMap {
     #[allow(clippy::if_same_then_else)]
     #[allow(clippy::needless_bool)]
-    pub fn risk_level_sum(&self) -> i32 {
+    fn low_points(&self) -> Vec<(usize, usize)> {
         (0..self.0.num_rows())
             .cartesian_product(0..self.0.num_columns())
             .filter(|&(r, c)| {
@@ -56,8 +57,50 @@ impl HeightMap {
                     true
                 }
             })
+            .collect::<Vec<_>>()
+    }
+
+    pub fn risk_level_sum(&self) -> i32 {
+        self.low_points()
+            .into_iter()
             .map(|coords| self.0[coords] as i32 + 1)
             .sum()
+    }
+
+    fn basin_sizes(&self) -> Vec<usize> {
+        let mut basin_sizes = Vec::new();
+        let low_points = self.low_points();
+        for (r, c) in low_points {
+            let mut visit = VecDeque::new();
+            let mut seen = HashSet::new();
+            visit.push_back((r, c));
+            while !visit.is_empty() {
+                let (r, c) = visit.pop_front().unwrap();
+                seen.insert((r, c));
+                for adjacent in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                    let next = (r as i32 + adjacent.0, c as i32 + adjacent.1);
+                    if next.0 < 0 || next.1 < 0 {
+                        continue;
+                    }
+                    let next = (next.0 as usize, next.1 as usize);
+                    if next.0 >= self.0.num_rows() || next.1 >= self.0.num_columns() {
+                        continue;
+                    }
+                    if self.0[next] == 9 || seen.contains(&next) {
+                        continue;
+                    }
+                    visit.push_back(next);
+                }
+            }
+            basin_sizes.push(seen.len());
+        }
+        basin_sizes
+    }
+
+    pub fn largest_basins_product(&self, num: usize) -> usize {
+        let mut sizes = self.basin_sizes();
+        sizes.sort_by(|i, j| j.partial_cmp(i).unwrap());
+        sizes.into_iter().take(num).product()
     }
 }
 
@@ -76,5 +119,11 @@ mod tests {
     fn test_risk_level_sum() {
         let height_map = INPUT.parse::<HeightMap>().unwrap();
         assert_eq!(15, height_map.risk_level_sum());
+    }
+
+    #[test]
+    fn test_largest_basins_product() {
+        let height_map = INPUT.parse::<HeightMap>().unwrap();
+        assert_eq!(1134, height_map.largest_basins_product(3));
     }
 }
